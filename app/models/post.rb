0026cc22a360db0602fc89837cea72a86e005387
Mari Taksa <welch.mari@gmail.com>
2021-08-31 10:21:24 -0500

    commit tree 92f2e6356808e5c9d652807309212b1cfcc91677

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :is_clickbait
+
+    def is_clickbait
+        unless title.present? && title.match?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+            errors.add(:title, "No journalistic integrity allowed")
+        end
+    end
 end
